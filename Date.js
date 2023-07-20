16990090e1239596331fe58db85d72d4b7db7c82
Mohsinkhan <57690391+mosimkha@users.noreply.github.com>
2023-07-21 00:35:33 +0530

    add reverse-string

--- Date.js
+++ Date.js
@@ -2,6 +2,10 @@ const Sub = (x, y) => {
     return console.log(x - y)
 }
 Add(9, 8)
+function reverseString(str1) {
+    return str1;
+}
+reverseString("hello1");
 console.log("date update");
 const Add = (a, b) => {
     return console.log(a + b)
