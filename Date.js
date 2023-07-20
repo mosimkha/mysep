06cd9ffc737b7398676444a5e2166ffce398ab68
Mohsinkhan <57690391+mosimkha@users.noreply.github.com>
2023-07-21 00:37:46 +0530

    reverse string2

--- Date.js
+++ Date.js
@@ -6,8 +6,19 @@ const Sub = (x, y) => {
     return console.log(x - y)
 }
 Add(9, 8)
+
+function reverse2(str) {
+    let r = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        r += str[i];
+    }
+    return r;
+}
+
+console.log(reverse2("javascript"))
 console.log("date update");
 const Add = (a, b) => {
     return console.log(a + b)
 }
 Add(2, 3)
+
